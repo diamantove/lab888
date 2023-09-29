6310417594cb662b3266b9ab00508d28dc36d094
Рустам Уджуху <rustam.u2020@mail.ru>
2023-09-29 18:22:35 +0300

    тили тили бом

--- time.pas
+++ time.pas
@@ -2,6 +2,6 @@
   var n := ReadInteger('Введите кол-во дней в году:');
   
   if (n mod 4 = 0) and not ((n mod 100 = 0) and (n mod 400 <> 0)) then
-    print('Год - високосный.')
-  else println('Год - не високосный.')
+    print('Год - високосный. Дней: 366.')
+  else println('Год - не високосный. Дней: 365.')
 end.
